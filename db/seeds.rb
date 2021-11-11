28ff7e99eef59b206336fbc007501cca98f6b140
Chad Cothran <ctcothran3@gmail.com>
2021-11-11 16:22:14 -0600

    seeded categories and created simple categories page and ads filtered by category page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Descriptions need to be better
 vehicles = Category.create!(
     title: 'Vehicles',
     description: 'Buy an entire car'
 )
+
+parts_mechanics = Category.create!(
+    title: 'Parts - Mechanics',
+    description: 'Mechanical Parts for sale'
+)
+
+parts_interior = Category.create!(
+    title: 'Parts - Interior',
+    description: 'Things that you can put inside your car'
+)
+parts_exterior = Category.create!(
+    title: 'Parts - Exterior',
+    description: 'Things that you can put outside your car'
+)
+tires = Category.create!(
+    title: 'Tires',
+    description: 'Tires of all kinds'
+)
+accessories = Category.create!(
+    title: 'Accessories',
+    description: 'Personalize your car with these things'
+)
 mechanics = Category.create!(
     title: 'Mechanics',
     description: 'These people can help fix your car up'
+)
+other_services = Category.create!(
+    title: 'Other Services',
+    description: 'Anything that does not fall into a category'
 )
 
 Ad.create!(
@@ -22,4 +50,31 @@ Ad.create!(
     price: 5650.0,
     status: 'active',
     category: vehicles
+)
+Ad.create!(
+    title: 'Nissan Altima',
+    description: 'White, 72450 miles, brand new tires',
+    location: 'Memphis, TN',
+    contact: 'bob@email.com',
+    price: 8500.0,
+    status: 'active',
+    category: vehicles
+)
+Ad.create!(
+    title: 'Black Phone Mount',
+    description: 'Mount up your phone to make using GPS that much easier',
+    location: 'Memphis, TN',
+    contact: 'alice@email.com',
+    price: 25.0,
+    status: 'active',
+    category: accessories
+)
+Ad.create!(
+    title: 'Camo Steering Wheel Cover',
+    description: 'Just buy it',
+    location: 'Memphis, TN',
+    contact: 'alice@email.com',
+    price: 10.0,
+    status: 'active',
+    category: accessories
 )
