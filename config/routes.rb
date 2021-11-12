fe8810399c4bb73df4bfa4be051e3978eef0e00a
Chad Cothran <ctcothran3@gmail.com>
2021-11-11 20:20:18 -0600

    added ad information page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'categories', to: 'categories#index', as: 'categories'
-  get 'categories/:id', to: 'categories#show', as: 'category'
+  
+  # Categories
+  get 'categories', to: 'categories#index', 
+  as: 'categories'
+  get 'categories/:id', to: 'categories#show', 
+  as: 'category'
+
+  # Ads
+  get 'categories/:category_id/:id', to: 'ads#show', 
+  as: 'ad'
 end
