d25cf50d5592fbff2c914bb64b2d53421f18cbd2
Chad Cothran <ctcothran3@gmail.com>
2021-11-17 14:23:53 -0600

    fixed duped seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_040300) do
+ActiveRecord::Schema.define(version: 2021_11_17_182713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,41 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-#USERS
-
-user1 = User.create!(
-    username: "badassBob",
-    first_name: "Bob",
-    last_name: "Hill",
-    email: "bboy@email.com",
-    phone: "100-234-5678",
-    bio: "I got the coolest parts in town!",
-    address1: "1111 driving way",
-    address2: " ",
-    address3: " ",
-    city: "NYC",
-    state: "NY",
-    country: "USA",
-    zip: 10001
-)
-
-user2 = User.create!(
-    username: "AngryAlice",
-    first_name: "Alice",
-    last_name: "Bezos",
-    email: "AtoZ@email.com",
-    phone: "200-432-4321",
-    bio: "Exotics only.",
-    address1: "2222 star road",
-    address2: " ",
-    address3: " ",
-    city: "Los Angeles",
-    state: "CA",
-    country: "USA",
-    zip: 90210
-)
-
-
 # CATEGORIES
 # Descriptions need to be better
 vehicles = Category.create!(
