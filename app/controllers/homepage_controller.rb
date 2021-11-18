b9413def734f9dd5d677255ba03af0d0ef71c6b6
Thomas Shelton <thomasearl09@gmail.com>
2021-11-18 11:43:31 -0600

    Update for working homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,10 @@
 class HomepageController < ApplicationController
     def home
         @hpcategories = Category.all
+        # logger"-----#{params[:category_id]}---"
+        # @hpcategory = Category.find(params[:category_id])
+        # @hpads = @hpcategory.ads.all
+        # @hpad = @hpcategory.ads.find(params[:id])
         render :home
     end
 end
