1b6f442d639a9e9795f9842d7406fb7219617eeb
meowth <flamingtoiletpaper@gmail.com>
2021-11-21 17:13:57 -0600

    made devise compatible with additional parameters, changed navbar link when signed in to redirect to user show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 
     add_flash_types :info, :error, :warning
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country, :state, :city, :address1, :address2, :address3, :zip, :bio, :password])
+    end
     
 end
